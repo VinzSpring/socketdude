2d743ac0a56f0f339a60c84701d543a656fbf566
VinzSpring <Rhinos12nacht!>
2019-05-16 13:56:46 +0200

    re init

--- src/obj/EVENTS.js
+++
@@ -1,8 +0,0 @@
-const EVENTS = {
-    ON_SOCKET_CLICKED: "0",
-    ON_PROJECT_CLICKED: "1",
-    ON_DELETE_SOCKET: "2",
-    ON_DELETE_PROJECT: "3",
-};
-
-export default EVENTS;
--- vueProj/README.md
+++ vueProj/README.md
@@ -1,4 +1,4 @@
-# socketdude
+# vueProj
 
 ## Project setup
 ```
