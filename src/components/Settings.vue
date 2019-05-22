f3048d7876236f1a321de703b1155d928582de52
Jonas Lieber <j.lieber@kreitiv.de>
2019-05-22 15:03:23 +0200

    moved v-card into mainview

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,17 +1,15 @@
 <template>
-  <v-card flat>
-    <v-layout align-center justify-center>
-      <v-flex xs3 mr-3>
-        <v-text-field label="Url"></v-text-field>
-      </v-flex>
-      <v-flex xs3 mr-3>
-        <v-select label="Protocol" :items="protocols" multiple></v-select>
-      </v-flex>
-        <v-btn icon flat>
-          <v-icon>cached</v-icon>
-        </v-btn>
-    </v-layout>
-  </v-card>
+  <v-layout align-center justify-center fill-height>
+    <v-flex xs3 mr-3>
+      <v-text-field label="Url"></v-text-field>
+    </v-flex>
+    <v-flex xs3 mr-3>
+      <v-select label="Protocol" :items="protocols" multiple></v-select>
+    </v-flex>
+    <v-btn icon flat>
+      <v-icon>cached</v-icon>
+    </v-btn>
+  </v-layout>
 </template>
 
 <script lang="ts">
@@ -20,8 +18,8 @@ export default Vue.extend({
   name: "Settings",
   data() {
     return {
-      protocols: [],
-    }
+      protocols: []
+    };
   }
 });
 </script>
