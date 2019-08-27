427935232d13cbb89d35c57ba03d7a4fd2733170
Vinz Spring <contact@vinzspring.de>
2019-08-27 23:22:11 +0200

    added logo, splashscreen

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,26 +1,29 @@
 <template>
   <v-container fluid pa-0 ma-0 grid-list>
+    <v-dialog v-model="showSplashScreen" width="50%" height="50%">
+      <SplashScreen></SplashScreen>
+    </v-dialog>
     <v-layout column align>
       <v-flex align-center>
         <v-card height="10vh" flat>
-          <Settings/>
+          <Settings />
         </v-card>
       </v-flex>
 
       <v-layout row fill-height>
         <v-flex xs3>
           <v-card flat class="projectListContainer" height="90vh">
-            <ProjectList/>
+            <ProjectList />
           </v-card>
         </v-flex>
         <v-flex xs5>
           <v-card flat height="90vh">
-            <ResponseComponent :activators="activators"/>
+            <ResponseComponent />
           </v-card>
         </v-flex>
         <v-flex xs4>
           <v-card flat height="90vh">
-            <ChatView :messages="messages"/>
+            <ChatComponent />
           </v-card>
         </v-flex>
       </v-layout>
@@ -29,33 +32,38 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import ChatView from '@/components/ChatComponent.vue';
-import Settings from '@/components/Settings.vue';
-import ProjectList from '@/components/ProjectList';
-import ResponseComponent from '@/components/ResponseComponent.vue';
-
-import { MESSAGES, ACTIVATORS } from '@/util/test-data';
+import Vue from "vue";
+import ChatComponent from "@/components/ChatComponent.vue";
+import Settings from "@/components/Settings.vue";
+import ProjectList from "@/components/ProjectList";
+import ResponseComponent from "@/components/ResponseComponent.vue";
+import SplashScreen from "@/components/SplashScreen.vue";
 
 export default Vue.extend({
-  name: 'MainView',
+  name: "MainView",
   components: {
-    ChatView,
+    ChatComponent,
     Settings,
     ProjectList,
     ResponseComponent,
+    SplashScreen
+  }, 
+  mounted() {
+    this.showSplashScreen = true;
+    window.setTimeout(() => {
+      this.showSplashScreen = false;
+    }, 3500);
   },
   data() {
     return {
-      messages: MESSAGES, // testing
-      activators: ACTIVATORS, // testing
+      showSplashScreen: false
     };
-  },
+  }
 });
 </script>
 
 <style scoped>
 .projectListContainer {
-  border-right: 1px dotted; 
+  border-right: 1px dotted;
 }
 </style>
